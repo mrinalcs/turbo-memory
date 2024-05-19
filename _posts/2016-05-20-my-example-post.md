f6828c4f87cdf970bd0f53883c7840a91918b957
mrinalcs <mrinalvbu@gmail.com>
2024-05-20 00:38:18 +0530

    add mermaid

--- _posts/2016-05-20-my-example-post.md
+++ _posts/2016-05-20-my-example-post.md
@@ -4,9 +4,77 @@ layout: post
 
 Eos eu docendi tractatos sapientem, brute option menandri in vix, quando vivendo accommodare te ius. Nec melius fastidii constituam id, viderer theophrastus ad sit, hinc semper periculis cum id. Noluisse postulant assentior est in, no choro sadipscing repudiandae vix. Vis in euismod delenit dignissim. Ex quod nostrum sit, suas decore animal id ius, nobis solet detracto quo te.
 
+ 
+```mermaid
+graph TD;
+    A-->B;
+    A-->C;
+    B-->D;
+    C-->D;
+```  
+
+```mermaid
+sequenceDiagram
+    Alice ->> Bob: Hello Bob, how are you?
+    Bob-->>John: How about you John?
+    Bob--x Alice: I am good thanks!
+    Bob-x John: I am good thanks!
+    Note right of John: Bob thinks a long<br/>long time, so long<br/>that the text does<br/>not fit on a row.
+
+    Bob-->Alice: Checking with John...
+    Alice->John: Yes... John, how are you?
+
+```
+
+```mermaid
+sequenceDiagram
+    loop Daily query
+        Alice->>Bob: Hello Bob, how are you?
+        alt is sick
+            Bob->>Alice: Not so good :(
+        else is well
+            Bob->>Alice: Feeling fresh like a daisy
+        end
+
+        opt Extra response
+            Bob->>Alice: Thanks for asking
+        end
+    end
+
+```
+
+
+
+
+```mermaid
+xychart-beta
+    title "Sales Revenue"
+    x-axis [jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec]
+    y-axis "Revenue (in $)" 4000 --> 11000
+    bar [5000, 6000, 7500, 8200, 9500, 10500, 11000, 10200, 9200, 8500, 7000, 6000]
+    line [5000, 6000, 7500, 8200, 9500, 10500, 11000, 10200, 9200, 8500, 7000, 6000]
+
+```
 {% comment %}
 Might you have an include in your theme? Why not try it here!
 {% include my-themes-great-include.html %}
 {% endcomment %}
 
 No laudem altera adolescens has, volumus lucilius eum no. Eam ei nulla audiam efficiantur. Suas affert per no, ei tale nibh sea. Sea ne magna harum, in denique scriptorem sea, cetero alienum tibique ei eos. Labores persequeris referrentur eos ei.
+
+```mermaid
+gitGraph:
+    commit "Ashish"
+    branch newbranch
+    checkout newbranch
+    commit id:"1111"
+    commit tag:"test"
+    checkout main
+    commit type: HIGHLIGHT
+    commit
+    merge newbranch
+    commit
+    branch b2
+    commit
+
+```
--- assets/js/swup.js
+++ assets/js/swup.js
@@ -17,19 +17,18 @@ const swup = new Swup({
 });
 
 
+    // Flag to indicate if MathJax has been loaded
+    let mathJaxLoaded = false;
 
-// Flag to indicate if MathJax has been loaded
-let mathJaxLoaded = false;
-
-// Function to initialize MathJax
-function initMathJax() {
-  if (!mathJaxLoaded && document.body.innerText.match(/\\\(|\\\[/)) {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.async = true;
-    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
-    document.head.appendChild(script);
-    mathJaxLoaded = true;
+    // Function to initialize MathJax
+    function initMathJax() {
+      if (!mathJaxLoaded && document.body.innerText.match(/\\\(|\\\[/)) {
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+        document.head.appendChild(script);
+        mathJaxLoaded = true;
 
         // Apply MathJax options
         window.MathJax = {
@@ -38,18 +37,47 @@ function initMathJax() {
           }
         };
 
-  } else if (window.MathJax) {
-    MathJax.typeset(); // Re-render math on subsequent transitions
-  }
-}
+      } else if (window.MathJax) {
+        MathJax.typeset(); // Re-render math on subsequent transitions
+      }
+    }
 
-// Function to initialize on initial page load
-function init() {
-  initMathJax();
-}
+    // Function to initialize Mermaid
+    function initMermaid() {
+      if (document.querySelector('.language-mermaid')) {
+        // Load the Mermaid library dynamically
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.src = 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.min.js';
+        document.head.appendChild(script);
+        
+        // Initialize Mermaid after the library is loaded
+        script.onload = function() {
+          document.querySelectorAll('pre code.language-mermaid').forEach((code) => {
+            // Create a <div class="mermaid"> element
+            const div = document.createElement('div');
+            div.className = 'mermaid';
+            div.textContent = code.textContent;
+          
+            // Replace the <pre><code> with the <div class="mermaid">
+            code.parentNode.replaceWith(div);
+          
+            // Render the Mermaid diagram
+            mermaid.init(undefined, div);
+          });
+        };
+      }
+    }
 
-// Call init() on initial page load
-init();
+    // Function to initialize on initial page load
+    function init() {
+      initMathJax();
+      initMermaid();
+    }
 
-// Call init() on each page transition
-swup.hooks.on('page:view', init);
+    // Call init() on initial page load
+    document.addEventListener('DOMContentLoaded', init);
+
+    // Call init() on each page transition
+    swup.hooks.on('page:view', init);
