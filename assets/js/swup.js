1be9d40286b0be22e47a5f29e4d546ed39b3c7a5
mrinalcs <mrinalvbu@gmail.com>
2024-05-27 19:51:07 +0530

    Update swup.js

--- assets/js/swup.js
+++ assets/js/swup.js
@@ -3,14 +3,13 @@ permalink: /script
 ---
 
 import Swup from 'https://unpkg.com/swup@4?module';
-import SwupHeadPlugin from 'https://unpkg.com/@swup/head-plugin@2?module';
 import SwupPreloadPlugin from 'https://unpkg.com/@swup/preload-plugin@3?module';
 import SwupScrollPlugin from 'https://unpkg.com/@swup/scroll-plugin@3?module';
 
 const swup = new Swup({
   containers: ["main"],
   plugins: [new SwupPreloadPlugin({ preloadVisibleLinks: true }),
-    new SwupHeadPlugin(),
+    
   new SwupScrollPlugin({
     animateScroll: false,
     shouldResetScrollPosition: (link) => !link.matches('.backlink')
@@ -19,58 +18,73 @@ const swup = new Swup({
 });
 
 
-    // Flag to indicate if MathJax has been loaded
-    let mathJaxLoaded = false;
-
-    // Function to initialize MathJax
-    function initMathJax() {
-      if (!mathJaxLoaded && document.body.innerText.match(/\\\(|\\\[/)) {
-        const script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.async = true;
-        script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
-        document.head.appendChild(script);
-        mathJaxLoaded = true;
-
-        // Apply MathJax options
-        window.MathJax = {
-          options: {
-            enableMenu: false // Disable the context menu
-          }
-        };
-
-      } else if (window.MathJax) {
-        MathJax.typeset(); // Re-render math on subsequent transitions
-      }
+// Function to initialize MathJax
+function initMathJax() {
+  // Check for mathematical syntax in the page content
+  if (document.body.innerHTML.match(/\\\(|\\\[|\\begin{.*?}/)) {
+    if (typeof MathJax === 'undefined') {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.async = true;
+      script.src = '/assets/js/MathJax.js?config=TeX-AMS_CHTML-full';
+      script.onload = () => {
+        MathJax.Hub.Config({
+          tex2jax: {
+            inlineMath: [['\\(','\\)']],
+            displayMath: [['\\[','\\]']],
+            processEscapes: true
+          },
+          showProcessingMessages: false,
+          messageStyle: 'none'
+        });
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub]); // Initial typeset after loading
+      };
+      document.head.appendChild(script);
+    } else {
+      MathJax.Hub.Queue(["Typeset", MathJax.Hub]); // Re-render math on subsequent transitions
     }
+  }
+}
+
 
     // Function to initialize Mermaid
-    function initMermaid() {
-      if (document.querySelector('.language-mermaid')) {
-        // Load the Mermaid library dynamically
-        const script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.async = true;
-        script.src = 'https://cdn.jsdelivr.net/npm/mermaid@10.9.1/dist/mermaid.min.js';
-        document.head.appendChild(script);
-        
-        // Initialize Mermaid after the library is loaded
-        script.onload = function() {
-          document.querySelectorAll('pre code.language-mermaid').forEach((code) => {
-            // Create a <div class="mermaid"> element
-            const div = document.createElement('div');
-            div.className = 'mermaid';
-            div.textContent = code.textContent;
-          
-            // Replace the <pre><code> with the <div class="mermaid">
-            code.parentNode.replaceWith(div);
-          
-            // Render the Mermaid diagram
-            mermaid.init(undefined, div);
-          });
-        };
-      }
+  // Function to initialize Mermaid
+function initMermaid() {
+  // Check if there are elements with the class '.language-mermaid'
+  if (document.querySelector('.language-mermaid')) {
+    // Check if Mermaid is already defined
+    if (typeof mermaid === 'undefined') {
+      // Load the Mermaid library dynamically
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.async = true;
+      script.src = '/assets/js/mermaid.min.js';
+      document.head.appendChild(script);
+      
+      // Initialize Mermaid after the library is loaded
+      script.onload = function() {
+        // Replace <pre><code> with <div class="mermaid">
+        document.querySelectorAll('pre code.language-mermaid').forEach((code) => {
+          const div = document.createElement('div');
+          div.className = 'mermaid';
+          div.textContent = code.textContent;
+          code.parentNode.replaceWith(div);
+          mermaid.init(undefined, div); // Render the Mermaid diagram
+        });
+      };
+    } else {
+      // Replace <pre><code> with <div class="mermaid">
+      document.querySelectorAll('pre code.language-mermaid').forEach((code) => {
+        const div = document.createElement('div');
+        div.className = 'mermaid';
+        div.textContent = code.textContent;
+        code.parentNode.replaceWith(div);
+        mermaid.init(undefined, div); // Render the Mermaid diagram
+      });
     }
+  }
+}
+
 
     // Function to initialize on initial page load
     function init() {
